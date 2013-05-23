3b36799903e0716e42dc94b56c7fd5d7e228f19f
LukaszFoj <foju1991@gmail.com>
2013-05-24 00:11:39 +0200

    projekt ruby

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubyface::Application.config.secret_token = '3734f044e534ee2f6eef7e03e80245549fa7addd5195f8fdd63fa76ff72682fe3e69348f8103d05f41e439cc713610571a7466b87c343ad4f3ba547a0f114659'
